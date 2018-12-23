84b852a4e9ef04abcf7bd9478a5a379ef556784f
mohamadjavad76 <46083077+mohamadjavad76@users.noreply.github.com>
2018-12-23 12:42:08 +0330

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+insert into teacher (TID,tname,tfamily)
+values ('8','asghar','bofeh');
+select * from teacher
+update teacher set tname = 'asgharr'
+where tname = 'asghar'
+delete from teacher
+select * from teacher
+where tname = 'asgharr' 
+select * from teacher
+select * from student
